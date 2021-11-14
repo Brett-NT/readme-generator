b3d530924936796f7bbbd5c99e0277bf5e2f3c1b
Brett Howard <howardcompositions@gmail.com>
2021-11-13 22:06:56 -0700

    Add fs.writeFile

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(questions.license == 'Unlicensed') {
+    return '';
+  }
+  return `
+  ![badge] (https://img.shields.io/badge/license-${questions.license}-brightgreen)<br />`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const badge = `![badge] (https://img.shields.io/badge/license-${questions.license}-brightgreen)`
+
+  return badge
+  `<br />
+  This app is using the ${questions.license} license.`
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(questions.license == 'Unlicensed') {
+    return ''
+  }
+  
+  return `
+  ## License`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
